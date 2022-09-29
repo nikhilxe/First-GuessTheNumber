2cc5dce2a145f52518e89acff3cddfe0842f90b8
nikhilxe <72465503+nikhilxe@users.noreply.github.com>
2022-09-29 15:17:47 +0530

    done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let SecretNumber=Math.trunc(Math.random()*20)+1;
 
 let score=20;
+let highscore=0;
 document.querySelector('.check').addEventListener('click', function() {
     const guess=Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
@@ -14,29 +15,24 @@ document.querySelector('.check').addEventListener('click', function() {
        
         document.querySelector('body').style.backgroundColor='#60b347';
         document.querySelector('.number').style.width='30rem';
-    }else if(guess > SecretNumber){
-        if(score>0){
-        document.querySelector('.message').textContent='too high';
-        score--;
-       document.querySelector('.score').textContent=score;} else{
-        document.querySelector('.message').textContent='you lost';
-        document.querySelector('.number').textContent=SecretNumber;
-        document.querySelector('.score').textContent=0;
-        document.querySelector('body').style.backgroundColor='rgb(216, 23, 23)';
-        document.querySelector('.number').style.width='30rem';
-       } 
-    }else if(guess < SecretNumber){
-        if(score>0){
-        document.querySelector('.message').textContent='too low';
-        score--;
-        document.querySelector('.score').textContent=score; }else{
-            document.querySelector('.message').textContent='you lost';
-            document.querySelector('.number').textContent=SecretNumber;
-            document.querySelector('.score').textContent=0;
-            document.querySelector('body').style.backgroundColor='rgb(216, 23, 23)';
-            document.querySelector('.number').style.width='30rem';
+        if(score>highscore){
+            highscore=score;
+            document.querySelector('.highscore').textContent=highscore;
         }
-    } 
+     }else if(guess!==SecretNumber){
+        if(score>1){
+            document.querySelector('.message').textContent=guess>SecretNumber ? 'too high 💹' : 'too low 📉';
+            score--;
+            document.querySelector('.score').textContent=score;
+        }else {
+            document.querySelector('.message').textContent='you lost the game!! 😫';
+         document.querySelector('.score').textContent=0;
+         document.querySelector('body').style.backgroundColor='rgb(218, 38, 38)';
+         document.querySelector('.number').style.width='30rem';
+
+        }
+     }
+   
 }); 
 document.querySelector('.again').addEventListener('click',function() {
     score=20;
